61f2ac110eb6c7a1d966cd3ba3304c502c223bfa
h4r5h1l <h4r5h1l@gmail.com>
2026-04-08 00:42:25 -0400

    ch2 Message Brokers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	conn_str := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(conn_str)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Connected to RabbitMQ successfully!")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
