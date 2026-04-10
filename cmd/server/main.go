6db5043ab512514a2dc76679548210e14d64645d
h4r5h1l <h4r5h1l@gmail.com>
2026-04-09 23:12:29 -0400

    ch3 l1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,9 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 	"os"
 	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 func main() {
@@ -15,8 +18,15 @@ func main() {
 		fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
 		return
 	}
-	defer conn.Close()
+	rmqChannel, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Failed to create channel: %s\n", err)
+		return
+	}
+	defer rmqChannel.Close()
 	fmt.Println("Connected to RabbitMQ successfully!")
+	state := routing.PlayingState{IsPaused: true}
+	pubsub.PublishJSON(rmqChannel, routing.ExchangePerilDirect, routing.PauseKey, state)
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
